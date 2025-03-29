ca8658ba7e4afae88bffeb058fc5f3a356ff40f1
Josh Levine <josh.levine1@gmail.com>
2025-03-29 16:47:07 -0700

    Add embeddings request

---
+++ Sources/FireworksAI-Swift/Requests/EmbeddingsRequest.swift
@@ -0,0 +1,43 @@
+//
+//  EmbeddingsRequest.swift
+//  FireworksAI
+//
+//  Created by Joshua Levine on 3/29/25.
+//
+
+import Foundation
+
+public struct EmbeddingsRequest: Codable, FireworksApiRequest {
+    public var path: String { "embeddings" }
+    public var method: String { "POST" }
+    
+    public var input: String
+    public var model: String
+    public var dimensions: Int
+    
+    public init(input: String, model: String, dimensions: Int) {
+        self.input = input
+        self.model = model
+        self.dimensions = dimensions
+    }
+}
+
+extension EmbeddingsRequest {
+    public struct Response: Codable, Sendable {
+        public var data: [Embedding]
+        public var model: String
+        public var object: String
+        public var usage: Usage
+    }
+    
+    public struct Embedding: Codable, Sendable {
+        public var index: Int
+        public var embedding: [Double]
+        public var object: String
+    }
+    
+    public struct Usage: Codable, Sendable {
+        public var prompt_tokens: Int
+        public var total_tokens: Int
+    }
+}
